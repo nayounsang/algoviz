68d7420c87612b750c4eab0a4ee4c73b52139c2a
nayounsang <nayounsang722@gmail.com>
2023-09-11 23:13:04 +0900

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NoImplement from "./component/page/NoImplement";
 import NotFound from "./component/page/NotFound";
 import UdGraphPage from "./component/page/UdGraphPage";
 import DGraphPage from "./component/page/DGraphPage";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, BrowserRouter, Route, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import MainPage from "./component/page/MainPage";
@@ -13,7 +13,23 @@ import HelpPage from "./component/page/HelpPage";
 const App = () => {
   return (
     <Provider store = {store}>
-      <BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/udgraph" element={<UdGraphPage />} />
+          <Route path="/dgraph" element={<DGraphPage />} />
+          <Route path="/tree" element={<NoImplement />} />
+          <Route path="/network" element={<NoImplement />} />
+          <Route path="/help" element={<HelpPage/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
+  );
+}
+/*
+<Provider store = {store}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/udgraph" element={<UdGraphPage />} />
@@ -25,7 +41,6 @@ const App = () => {
         </Routes>
       </BrowserRouter>
     </Provider>
-  );
-}
+*/
 
 export default App;
